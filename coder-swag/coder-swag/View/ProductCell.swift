c3e88278a623354c030b7f0ee306c17bf2be55f8
training <marcin.danilewicz@mobica.com>
2018-07-20 16:01:12 +0200

    5_58 coderswag collection views

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by training on 20.07.2018.
+//  Copyright © 2018 Developers. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+    
+    
+}
