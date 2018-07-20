c396f2bf2db3cd95ca28acebb1ab022f7201f6d2
training <marcin.danilewicz@mobica.com>
2018-07-20 17:35:09 +0200

    5_60 coderswag showing data in collection view

---
+++ coder-swag/coder-swag/Controller/ProductsVC.swift
@@ -0,0 +1,59 @@
+//
+//  ProductsVC.swift
+//  coder-swag
+//
+//  Created by training on 20.07.2018.
+//  Copyright © 2018 Developers. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var productsCollection: UICollectionView!
+
+    private(set) public var products = [Product]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+      productsCollection.dataSource = self
+      productsCollection.delegate = self
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func initialiseProducts(category: Category) {
+        self.products = DataService.instance.getProducts(forCategoryTitle_friendlyName: category.categoryTitle)
+        //extra stuff
+        navigationItem.title = category.categoryTitle
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      return products.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        return ProductCell()
+
+    }
+    
+    
+    
+}
